b24891759a4c98e1091caac8198c82206822608f
bakry <bakryhassona7@gmail.com>
2025-05-05 19:09:05 +0300

    Complete Generic Repository And Implemention

---
+++ Foodyz.Application/Contracts/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodyz.Application.Contracts.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>> filter = null , Func<IQueryable<T>,IOrderedQueryable<T>> orderby = null, string[] include = null);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy = null, string[] include = null);
+
+        IQueryable<T> Sync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy = null, string[] include = null);
+        Task<T> GetByIdAsync(int Id);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> IsExist(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ Foodyz.Infrastructure/Implemention/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Foodyz.Application.Contracts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodyz.Infrastructure.Implemention
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+    }
+}
